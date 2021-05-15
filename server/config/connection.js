c98b0e6b2cb6882387919dff800310e4f1e1c481
Smack <kovacsmyles1@gmail.com>
2021-05-15 17:25:46 -0500

    fix2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const dbAtlas = config.get('MONGODB_URI')
+const config = require('config');
 const mongoose = require('mongoose');
 
 mongoose.connect(dbAtlas || 'mongodb://localhost/mernshopping', {
